59b9d769907fe36e9c82447fdbace19a88b79fb2
Ray <rayfunahashi@gmail.com>
2021-01-13 22:09:26 -0500

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAXTbZkOkgVCqFrkBN0dAzUqk-PK9xqc1U",
+  authDomain: "challenge-63392.firebaseapp.com",
+  projectId: "challenge-63392",
+  storageBucket: "challenge-63392.appspot.com",
+  messagingSenderId: "670371574231",
+  appId: "1:670371574231:web:457528f6e0b8c1a32c9b92",
+};
