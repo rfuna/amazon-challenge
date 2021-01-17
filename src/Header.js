493a830d61c7775190edee2c1bd6f960ce99fc00
Ray <rayfunahashi@gmail.com>
2021-01-16 22:01:03 -0500

    third commit

--- src/Header.js
+++ src/Header.js
@@ -2,14 +2,17 @@ import React from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <div className="header">
+      <Link to='/'>
       <img
         src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
         className="header__logo"
       ></img>
+      </Link>
 
       <div className="header__search">
         <input type="text" className="header__searchInput" />
@@ -29,10 +32,12 @@ function Header() {
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
         </div>
+        <Link to="/checkout">
         <div className="header__optionBasket">
           <ShoppingBasketIcon />
           <span className="header__optionLineTwo header__basketCount">0</span>
         </div>
+        </Link>
       </div>
     </div>
   );
--- src/Home.js
+++ src/Home.js
@@ -13,20 +13,21 @@ function Home() {
         />
         <div className="home__row">
           <Product
+            id="121312314"
             title="KIOS Electric Juicer"
             price={89.99}
             image="https://m.media-amazon.com/images/I/41IX+BQZJGL._AC_SY240_.jpg"
             rating={5}
           />
-          <Product />
+          <Product id="12123332314" title="REMNANT Widescreen monitor" price={356.99} image="https://m.media-amazon.com/images/I/410B5vZE6IL._AC_SY200_.jpg" rating={4} />
         </div>
         <div className="home__row">
-          <Product />
-          <Product />
-          <Product />
+        <Product id="1212385714" title="Mega Black Metal Office desk" price={545.99} image="https://m.media-amazon.com/images/I/41gaeDOKkxL._AC_SY200_.jpg" rating={5} />
+        <Product id="12332387654" title="Traveler Luggage set" price={678.99} image="https://m.media-amazon.com/images/I/51kEMDfsoAL._AC_SY200_.jpg" rating={3} />
+        <Product id="1212547314" title="HP Deskjet 5437 printer" price={56.89} image="https://m.media-amazon.com/images/I/41dCZxQDk2L._AC_SY200_.jpg" rating={3} />
         </div>
         <div className="home__row">
-          <Product />
+        <Product id="12124332314" title="Handcrafted oak hangar set" price={34.99} image="https://m.media-amazon.com/images/I/41DgO9C7kQL._AC_SY200_.jpg" rating={4} />
         </div>
       </div>
     </div>
